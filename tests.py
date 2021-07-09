ba5c8d10c849e75f3c9afe0ffe1c9017f14f7137
hghiscock <hamishhiscock@gmail.com>
2021-07-09 15:34:28 +0100

    testing

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import spin_dynamics
 from numba import cuda
-#test commit
+
 #-----------------------------------------------------------------------------#
 #-----------------------------------------------------------------------------#
 
